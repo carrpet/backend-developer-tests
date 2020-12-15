119d0870f18c3e8e6d5c1a5b4abbcc7580506e7c
Peter Carr <carrpet@gmail.com>
2020-12-15 05:34:37 -0700

    initial unit test implementation with default test case

--- unit-testing/main.go
+++ unit-testing/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/stackpath/backend-developer-tests/unit-testing/pkg"
+	fizzbuzz "github.com/carrpet/backend-developer-tests/unit-testing/pkg"
 )
 
 func main() {
--- unit-testing/pkg/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz_test.go
@@ -1,7 +1,37 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+var defaultResult = []string{"1", "2", "Fizz", "4", "Buzz",
+	"Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16"}
+
+var testcases = []struct {
+	total  int64
+	fizzAt int64
+	buzzAt int64
+	result []string
+}{
+	{total: 16, fizzAt: 3, buzzAt: 5, result: defaultResult},
+}
 
 func TestFizzBuzz(t *testing.T) {
-	t.Skip("TODO: Add tests")
+
+	//var executeTests =
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("total: %d, fizzAt: %d, buzzAt: %d", tc.total, tc.fizzAt, tc.buzzAt),
+			func(t *testing.T) {
+				res := FizzBuzz(tc.total, tc.fizzAt, tc.buzzAt)
+				if len(res) != len(tc.result) {
+					t.Errorf("Expected result length %d, got length %d", len(tc.result), len(res))
+				}
+				for i, expected := range tc.result {
+					if res[i] != expected {
+						t.Errorf("Expected %s got %s at result position %d", expected, res[i], i)
+					}
+				}
+			})
+	}
 }
